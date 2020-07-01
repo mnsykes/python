5d647c1de53095ef5245bcd72197c7a402d4b510
Matthew Sykes <mnsykes@gmail.com>
2020-06-30 23:28:36 -0400

    dictionary

--- Week 7/sykes_lookup.py
+++ Week 7/sykes_lookup.py
@@ -1,21 +1,21 @@
-
-choice = input("Y or N:  ")
-if choice == 'y':
-  data = dict()
-  data['name'], data['street'], data['city'], data['state'], data['zip'], data['phone']  = [], [], [], [], [], []
-  with open('address.txt', 'r') as f:
+while True: 
+  choice = input("Would you like to use the lookup tool? Enter (y) for Yes or (n) for No: ")
+  if choice == 'y':
+    f = open('address.txt', 'r')
     for line in f:
-      keyValPairs = line.split(',')
-      for kvPair in keyValPairs:
-        kv = kvPair.strip().lower().split(':')
-        print(kv)
-        key = kv[0]
-        val = kv[1]
-        data[key].append(val)
-else:
-  print('You have chosen to quit the program')
+      fileList = line.strip().lower().split(',')
+      data = {}
+      data['Name'] = fileList[0]
+      data['Street'] = fileList[1]
+      data['City'] = fileList[2]
+      data['State'] = fileList[3]
+      data['Zip Code'] = fileList[4]
+      data['Phone'] = fileList[5]
+      name = input("Please enter a name: ")
+      if name in data:
+        print(data)
+  # else:
+  #   print('You have chosen to quit the program')
 
 
 
-
-# , data['street'], data['city'], data['state'], data['zip'], data['phone'] = [], [], [], [], [], []
