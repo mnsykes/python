e15364a26183fb748ce6acfeafa1acf3b13c1b2e
Matthew Sykes <mnsykes@gmail.com>
2020-07-04 14:04:34 -0400

    Dictionary created

--- Week 7/sykes_lookup.py
+++ Week 7/sykes_lookup.py
@@ -1,20 +1,21 @@
 def dictLookup():
-  data = {}
-
-  with open('address.txt', 'r') as f:
-    for line in f:
-      fileList = line.strip().lower().split(',')
-      key, values = 'name', fileList[0:]
-      data[key] = values
-      for key in data:
-        person = input('Enter a name: ')
-        if person in key:
-          print(data)
+  f = open('address.txt', 'r')
+  for line in f:
+    personalInfo = line.lower().strip().split(', ')
+    for value in personalInfo:
+      myDict = {}
+      myDict[personalInfo[0]] = personalInfo[1:]
+    for key in myDict:
+      print(key, myDict[key])
+    # print(myDict)
     
 
 choice = input('Would you like to use the lookup tool? Enter (y) for Yes or (n) for No: ').lower()
 if choice == 'y':
   dictLookup()
-
+  
+  
+  
+  
 
 
