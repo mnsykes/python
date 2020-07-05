caa797637c9bb66c7ab5b8ee8d7a2ab27d05a311
Matthew Sykes <mnsykes@gmail.com>
2020-07-04 22:51:35 -0400

    Completed:  function to open file and return dictionary.   To Do:  Function to format and main function among other things

--- Week 7/sykes_lookup.py
+++ Week 7/sykes_lookup.py
@@ -1,18 +1,27 @@
-def dictLookup():
-  f = open('address.txt', 'r')
-  for line in f:
-    personalInfo = line.lower().strip().split(', ')
-    for value in personalInfo:
-      myDict = {}
-      myDict[personalInfo[0]] = personalInfo[1:]
-    for key in myDict:
-      print(key, myDict[key])
-    # print(myDict)
-    
 
-choice = input('Would you like to use the lookup tool? Enter (y) for Yes or (n) for No: ').lower()
-if choice == 'y':
+def dictLookup():
+  try:
+    lookup = {}
+    f = open(choice, 'r')
+    for line in f:
+      personalInfo = line.lower().strip().split(', ')
+      lookup[personalInfo[0]] = personalInfo[1:]
+    return lookup
+
+  except FileNotFoundError:
+    print('Error: file did not open')
+if __name__== "_dictLookup_":
   dictLookup()
+
+
+choice = input('Enter a file to open: ').lower()
+print('\n')
+lookup = dictLookup()
+for key in lookup:
+  key = input('Enter a name: ').lower()
+  if key in lookup:
+    print(lookup[key])
+  
   
   
   
